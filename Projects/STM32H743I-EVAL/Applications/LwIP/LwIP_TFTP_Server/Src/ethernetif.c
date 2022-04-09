eb1a9d7850239360e10323decf3ab7f7b42ae6ac
forrest <forrestchu12@gmail.com>
2022-04-09 09:36:49 +0800

    fit to new board

--- Projects/STM32H743I-EVAL/Applications/LwIP/LwIP_TFTP_Server/Src/ethernetif.c
+++ Projects/STM32H743I-EVAL/Applications/LwIP/LwIP_TFTP_Server/Src/ethernetif.c
@@ -372,65 +372,43 @@ u32_t sys_now(void)
 void HAL_ETH_MspInit(ETH_HandleTypeDef *heth)
 {
   GPIO_InitTypeDef GPIO_InitStructure = {0};
-  /* Ethernett MSP init: RMII Mode
-
-  RX_CLK  --------------> PA1
-  TXD0    --------------> PB12
-  TXD1    --------------> PB13
+  
+  /* Ethernett MSP init: RMII Mode 
+  
+  RX_CLK  --------------> PA1  
+  TXD0    --------------> PB12  
+  TXD1    --------------> PB13  
   RXD0    --------------> PC4
   RXD1    --------------> PC5
-  TX_EN   --------------> PB11
+  TX_EN   --------------> PB11 
   RX_DV   --------------> PA7
-
+  
   MDC     --------------> PC1
   MDIO    --------------> PA2
-
+  
   */
-
+  
   /* Enable GPIOs clocks */
   __HAL_RCC_GPIOA_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
   __HAL_RCC_GPIOC_CLK_ENABLE();
-  __HAL_RCC_GPIOG_CLK_ENABLE();
-
-/* Ethernet pins configuration ************************************************/
-  /*
-        RMII_REF_CLK ----------------------> PA1
-        RMII_MDIO -------------------------> PA2
-        RMII_MDC --------------------------> PC1
-        RMII_MII_CRS_DV -------------------> PA7
-        RMII_MII_RXD0 ---------------------> PC4
-        RMII_MII_RXD1 ---------------------> PC5
-        RMII_MII_RXER ---------------------> PG2
-        RMII_MII_TX_EN --------------------> PG11
-        RMII_MII_TXD0 ---------------------> PG13
-        RMII_MII_TXD1 ---------------------> PB13
-  */
-
-  /* Configure PA1, PA2 and PA7 */
+  
+  /* Configure PA1, PA2 , PA7 */
+  GPIO_InitStructure.Pin =  GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_7;
   GPIO_InitStructure.Speed = GPIO_SPEED_FREQ_HIGH;
   GPIO_InitStructure.Mode = GPIO_MODE_AF_PP;
-  GPIO_InitStructure.Pull = GPIO_NOPULL; 
+  GPIO_InitStructure.Pull = GPIO_NOPULL ; 
   GPIO_InitStructure.Alternate = GPIO_AF11_ETH;
-  GPIO_InitStructure.Pin = GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_7;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStructure);
-
-  /* Configure PB13 */
-  GPIO_InitStructure.Pin = GPIO_PIN_13;
+  
+  /* Configure PG11, PG12 and PG13 */
+  GPIO_InitStructure.Pin = GPIO_PIN_11 | GPIO_PIN_12 | GPIO_PIN_13;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStructure);
-
+  
   /* Configure PC1, PC4 and PC5 */
-  GPIO_InitStructure.Pin = GPIO_PIN_1 | GPIO_PIN_4 | GPIO_PIN_5;
-  HAL_GPIO_Init(GPIOC, &GPIO_InitStructure);
-
-  /* Configure PG2, PG11, PG13 and PG14 */
-  GPIO_InitStructure.Pin =  GPIO_PIN_2 | GPIO_PIN_11 | GPIO_PIN_13;
-  HAL_GPIO_Init(GPIOG, &GPIO_InitStructure);	
-
-  /* Enable the Ethernet global Interrupt */
-  //HAL_NVIC_SetPriority(ETH_IRQn, 0x7, 0);
-  //HAL_NVIC_EnableIRQ(ETH_IRQn);
-
+  GPIO_InitStructure.Pin = GPIO_PIN_1 | GPIO_PIN_4 | GPIO_PIN_5; 
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStructure);	
+  
   /* Enable Ethernet clocks */
   __HAL_RCC_ETH1MAC_CLK_ENABLE();
   __HAL_RCC_ETH1TX_CLK_ENABLE();
